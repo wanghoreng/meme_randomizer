b2eccbf039aed534afa4eaf23e74f2f5f0b8710a
jihea <bictiger3@gamil.com>
2025-08-25 23:53:02 +0900

    0825 짤 랜덤 추천기 프로젝트 Start

---
+++ randomizer/src/main/java/com/meme/randomizer/RandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.meme.randomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomizerApplication.class, args);
+	}
+
+}
---
+++ randomizer/src/test/java/com/meme/randomizer/RandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meme.randomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
